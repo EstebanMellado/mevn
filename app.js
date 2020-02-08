ff26e59a23ce510e3d06cdedbf2b79473209e63e
esteban <melladoesteban@yahoo.com.ar>
2020-02-08 17:52:11 -0300

    add mongodb config

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const history = require('connect-history-api-fallback');
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://localhost:27017/mevn';
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+};
+
+mongoose.connect(uri, options).then(
+    () => { console.log('conectado a mongoDb') },
+    err => { err }
+);
 
 app.use(morgan('tiny'));
 app.use(cors());
